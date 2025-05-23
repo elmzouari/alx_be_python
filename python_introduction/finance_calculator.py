27de3f75e230262146775e7d69728c1b9e02d9db
younes el mezouari <109693920+elmzouari@users.noreply.github.com>
2025-05-23 18:37:42 +0100

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,3 +1,6 @@
-current_age = int(input("How old are you? "))
-future_age = current_age + (2050 - 2023)
-print(f"In 2050, you will be {future_age} years old")
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = monthly_savings * 12 * 1.05  # 12 months + 5% interest
+print(f"Your monthly savings are ${monthly_savings:.0f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.0f}")
